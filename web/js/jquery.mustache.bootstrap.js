d511b88d8ad49f380f2c9d9275fb52dfc37ff90f
Saidou GUEYE <saidou.gueye@gmail.com>
2015-10-23 13:38:39 -0400

    Change when the functions are defined to make sure libs are loaded when they run

--- web/js/jquery.mustache.bootstrap.js
+++ web/js/jquery.mustache.bootstrap.js
@@ -9,19 +9,10 @@
                 $.Mustache.addFromDom();
         });
 
-
-//        // Load a single template.
-//        $.Mustache.load('/demo/list.html')
-//                .done(function () {
-//                        var output = $('#demo-list');
-//
-//                        output.append($.Mustache.render('demo-list', { items: [
-//                                {name: "Foo"},
-//                                {name: "Bar"}
-//                            ] }));
-//                });
-           
+           //Load the list
            listItems();
+           
+           //Runs an Ajax call that updates the item. The controller will redirect to the list
            function updateItem(id)
            {
                 $.ajax('/app_dev.php/edit/'+id, {
@@ -36,7 +27,11 @@
            }
            
            
-           
+           /**
+            * Creates the list: runs an Ajax call and uses a moustache template
+            * to render the json result
+            * @returns {undefined}
+            */
            function listItems()
            {
                 $.ajax('/app_dev.php/list', {
@@ -52,13 +47,7 @@
                             output.find('.demo-delete').click(function(event) {
                                 event.preventDefault();
                                 deleteItem($(this).data('id'));
-                            });
-                            console.log("length="+$('#demo-address').length);
-           $('#demo-address').change(function(event) {
-               console.log("$(this).val()="+$(this).val());
-                pinAddress($(this).val());
-            });
-                            
+                            });          
 
                         });
                       },
@@ -68,6 +57,13 @@
                    });
            }
            
+           /**
+            * Deletes a given item.
+            * 
+            * It uses an Ajax call. After the result it rebuilds the list
+            * @param {type} id
+            * @returns {undefined}
+            */
            function deleteItem(id)
            {
                 $.ajax('/app_dev.php/delete/'+id, {
@@ -76,43 +72,8 @@
                     }
                 });
            }   
-           var gapiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDy0RByTQl7pQ1HwW9IxwgYmQemJKjUklA&address=';
-           function pinAddress(adr) {
-               console.log("adr="+adr);
-               $.ajax(gapiUrl+adr, {
-                    success: function(data) {
-                        var myLatLng = data.results[0].geometry.location;
-                        //{lat: -25.363, lng: 131.044};
-                        var map = new google.maps.Map(document.getElementById('map'), {
-                          zoom: 4,
-                          center: myLatLng
-                        });
-
-                        var marker = new google.maps.Marker({
-                          position: myLatLng,
-                          map: map,
-                          title: data.results[0].formatted_address
-                        });
-                    }
-                });
-           }
-           //pinAddress('1600+Amphitheatre+Parkway,+Mountain+View,+CA');
+           
+           //('1600+Amphitheatre+Parkway,+Mountain+View,+CA');
 
 
 })(jQuery);
-
-function initMap(adr) {
-    console.log('Init map');
-    var myLatLng = {lat: -25.363, lng: 131.044};
-//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAYDI2oJKL73riaHmA0j4S6trGlFpwkZE0
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: myLatLng
-    });
-
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Hello World!'
-    });
-}
